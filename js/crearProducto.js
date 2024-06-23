05850c7d178c3db82b976442dce6f05bca243813
LucasPeraltaa <lucasmperaltaa@gmail.com>
2024-06-23 02:31:24 -0300

    dimos marcha al archivo crearProducto, ya funciona la funcion enviarPorducto, pero tenemos que corregir error con el ID

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -4,7 +4,7 @@ const formulario = document.querySelector("[data-formulario]")
 
 async function crearCard(evento){
 
-    evento.preventDefault()
+    evento.preventDefault() //evita que se ejecute de forma automatica
 
     const nombre = document.querySelector("[data-nombre]").value
     const precio = document.querySelector("[data-precio]").value
